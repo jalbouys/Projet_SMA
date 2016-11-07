6114100b37319504a2575fec546e48103cfdcf8b
Tsumenokage <Jeremy.tsume@Gmail.com>
2016-11-07 12:31:05 +0100

    Début
    Début de projet, village et déplacement personnage

---
+++ Projet_SMA/Assets/MoveTo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+
+    public GameObject personnage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, personnage.transform.position, Time.deltaTime * 5);
+        transform.LookAt(personnage.transform.position);
+	}
+}
