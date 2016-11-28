fdbfd8fdf0eb0484a396de476bfc751b7afe300f
Tsumenokage <Jeremy.tsume@Gmail.com>
2016-11-28 10:53:34 +0100

    Ajout script
    Croise les doigts

--- Projet_SMA/Assets/Attack.cs
+++ Projet_SMA/Assets/Attack.cs
@@ -2,14 +2,35 @@
 using System.Collections;
 
 public class Attack : MonoBehaviour {
-    
+
+    public float nextAttackTime;
+    public float attackTime = 1;//time it takes to do 1 attack
+
     // Use this for initialization
     void Start () {
-	
-	}
+        nextAttackTime = Time.time;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        
+        GetInfo getInfo = GetComponent<GetInfo>();
+        var distance = Vector3.Distance(getInfo.target.transform.position, transform.position);
+        if (distance < 2)//if close enough, attack
+        {
+            if(Time.time > nextAttackTime)
+                attack(getInfo.target);//whoop-ass
+        }
+    }
+
+    void attack(GameObject target)
+    {
+        nextAttackTime = Time.time + attackTime;//time when the next attack can happen
+        GetInfo getInfo = target.GetComponent<GetInfo>();
+        getInfo.hp -= 10;//remove 10HP from victim
+        getInfo.attacker = transform.gameObject;
+        Debug.Log("Whack! ");
+        Debug.Log(getInfo.hp);
+        Debug.Log(" HP left\n");
     }
 }
